7f8643a384d8b69914a91d3ece49678fd490a29e
Velapsis <romain.Vicente@ynov.Com>
2024-12-31 14:41:05 +0100

    update

--- main.go
+++ main.go
@@ -1,25 +1,24 @@
 package main
 
-
 import (
-	"net/http"
 	"log"
+	"net/http"
 )
 
 func main() {
 
-fs := http.FileServer(http.Dir("static"))
-http.Handle("/static/", http.StripPrefix("/static/", fs))
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "static/Menu.html")
-	
-})
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "static/Menu.html")
 
-log.Println("Server started on http://localhost:8080")
-err := http.ListenAndServe(":8080", nil)
-if err != nil {
-	log.Fatal(err)
+	})
+
+	log.Println("Server started on http://localhost:8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
 
 	}
 }
