8413602967c31fe050290aceb7bef69ab845daf4
Nothypaa <Mahan.mir@ynov.Com>
2025-01-01 21:35:12 +0100

    adding minor changes and details

--- main.go
+++ main.go
@@ -1,25 +1,25 @@
 package main
 
-
 import (
-	"net/http"
 	"log"
+	"net/http"
 )
 
 func main() {
 
-fs := http.FileServer(http.Dir("static"))
-http.Handle("/static/", http.StripPrefix("/static/", fs))
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "static/Menu.html")
-	
-})
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control","no-store, no cache, must-revalidate, proxy-revalidate ")
+		http.ServeFile(w, r, "static/html/Menu.html")
 
-log.Println("Server started on http://localhost:8080")
-err := http.ListenAndServe(":8080", nil)
-if err != nil {
-	log.Fatal(err)
+	})
+
+	log.Println("Server started on http://localhost:8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
 
 	}
 }
