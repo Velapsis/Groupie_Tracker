0c03f469db5392298f2b859e8bb26b5ba667febb
Nothypaa <Mahan.mir@ynov.Com>
2025-01-22 21:00:10 +0100

    merged

--- static/js/mainpage.js
+++ static/js/mainpage.js
@@ -37,11 +37,28 @@ document.addEventListener("DOMContentLoaded", () => {
     animate();
 });
     
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.getElementById('back-to-top');
 
+    if (button) {
+        window.addEventListener('scroll', function () {
+            if (window.scrollY > 300) {
+                button.classList.add('show');
+            } else {
+                button.classList.remove('show');
+            }
+        });
 
-//NEEDS TO BE EDITED BY MAHAN !!!!!!
-
-
+        button.addEventListener('click', function () {
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });
+        });
+    } else {
+        console.error("Element with ID 'back-to-top' not found.");
+    }
+});
 
 
 
